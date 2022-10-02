433d89527c4d037286cd0c39ebef7298215941ec
Bryan | JJ <with.beth@gmail.com>
2022-10-02 15:36:21 +0900

    feat : add GameConfiguration object

---
+++ src/main/java/baseball/GameConfiguration.java
@@ -0,0 +1,10 @@
+package baseball;
+
+// TODO : Enum?
+public final class GameConfiguration {
+
+    public static final int MAX_BALL_SIZE = 3;
+    public static final int MIN_BALL_NO = 1;
+    public static final int MAX_BALL_NO = 9;
+
+}
